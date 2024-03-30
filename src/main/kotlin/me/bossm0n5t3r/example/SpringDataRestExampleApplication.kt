65b8b2746def24ecdd8f1a7e6f8fe5d284f3acb8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-03-31 03:33:00 +0900

    :rocket:

---
+++ src/main/kotlin/me/bossm0n5t3r/example/SpringDataRestExampleApplication.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataRestExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringDataRestExampleApplication>(*args)
+}
---
+++ src/test/kotlin/me/bossm0n5t3r/example/SpringDataRestExampleApplicationTests.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataRestExampleApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
